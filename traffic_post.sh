ab6be4e1ff3246d66de210c12140615aeb4bebef
ta08 <marl4dev@gmail.com>
2019-08-28 21:57:00 +0900

    update one line script to be readable

--- traffic_post.sh
+++ traffic_post.sh
@@ -1,4 +1,13 @@
 #/bin/sh
 
-/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I | tr -d ' ' | jq -R 'split(":") | {"key": .[0], "value": (.[1]|if test("^[+-]?(?:\\d+\\.?\\d*|\\.\\d+)+$") then tonumber else . end)}' | jq -r -s 'from_entries' | jq  --arg timestamp "$(date '+%Y-%m-%dT%H:%M:%S%z')" '. =.+{"createdAt": $timestamp}' | jq -c 'del(.channel)'| http POST localhost:9200/network_status/wifi
+airport () {
+  /System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I
+}
+
+airport | tr -d ' ' \
+| jq -R 'split(":") | {"key": .[0], "value": (.[1]|if test("^[+-]?(?:\\d+\\.?\\d*|\\.\\d+)+$") then tonumber else . end)}' \
+| jq -r -s 'from_entries' \
+| jq --arg timestamp "$(date '+%Y-%m-%dT%H:%M:%S%z')" '. =.+{"createdAt": $timestamp}' \
+| jq -c 'del(.channel)' \
+| http POST localhost:9200/network_status/wifi
 
